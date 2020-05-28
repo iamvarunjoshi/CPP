af463742471f64486d38f2bff237466d00494cc9
Varun Joshi <54651150+iamvarunjoshi@users.noreply.github.com>
2020-05-28 23:33:48 +0530

    Update and rename doc.py to doc.cpp

---
+++ c++/doc.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main()
+{
+std::cout<<"Hi all Find my solutions to various problems i've practiced till date"<<std::endl<<"Feel free to edit it and add to it Thanking you in advance.";
+  
+  
+return 0;
+}
