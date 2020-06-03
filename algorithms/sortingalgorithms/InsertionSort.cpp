36d81db2d7d2f8685ca4c1cd9ef76fe35b78e8dc
Varun Joshi <54651150+iamvarunjoshi@users.noreply.github.com>
2020-06-03 23:21:22 +0530

    Create InsertionSort.cpp

---
+++ algorithms/sortingalgorithms/InsertionSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#define MAXN 1000
+using namespace std;
+
+void insertionsort(int arr[], int ei)
+{
+	for (int i = 1; i <= ei; i++)
+	{
+		int j = i;
+		while (j > 0 && arr[j] < arr[j - 1])
+		{
+			int temp = arr[j - 1];
+			arr[j - 1] = arr[j];
+			arr[j] = temp;
+			j--;
+		}
+	}
+}
+
+int main()
+{
+	int arr[MAXN+2];
+
+	for (int i=MAXN;i>=0;i--)
+		arr[MAXN - i] = i;
+    
+	insertionsort(arr,MAXN);
+
+	for (int i = 0; i <=MAXN; i++)
+		cout<<arr[i]<<" ";
+
+
+}
