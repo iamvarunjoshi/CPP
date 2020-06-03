aeadb5ff929d957b2127eb479a1897cad49ebe19
Varun Joshi <54651150+iamvarunjoshi@users.noreply.github.com>
2020-06-03 23:14:35 +0530

    Create BubbleSort.cpp

---
+++ algorithms/sortingalgorithms/BubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#define MAXN 1000
+using namespace std;
+int temp[MAXN+2];
+
+void bubblesort(int arr[],int ei)
+{
+	for (int i = 0; i <= ei; i++)
+	{
+		for (int j = i + 1; j <= ei; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int arr[MAXN+2];
+
+	for (int i=MAXN;i>=0;i--)
+		arr[MAXN - i] = i;
+    
+	bubblesort(arr,MAXN);
+
+	for (int i = 0; i <=MAXN; i++)
+		cout<<arr[i]<<" ";
+}
